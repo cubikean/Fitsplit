c398bb1ef0004a945069b54b9e1da742f0b5a64a
cubikean <hugochatigny@gmail.com>
2022-03-12 14:17:17 +0100

    Menu - Update

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FitSplit - Accueil</title>
     <link rel="stylesheet" href="assets/scss/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <?php include 'template-parts/header.php';?>
-    <h1>hello</h1>
 </body>
 </html>
--- template-parts/header.php
+++ template-parts/header.php
@@ -2,23 +2,22 @@
     <div class="ft_nav_logo"><img src="https://picsum.photos/200/70" alt=""></div>
     <div class="ft_nav_items">
         <div class="ft_nav_item"><a href="">Les coachs</a></div>
-        <div class="ft_nav_item"><a href="">Les coachs</a></div>
-        <div class="ft_nav_item"><a href="">Les coachs</a></div>
-        <div class="ft_nav_item"><a href="">Les coachs</a></div>
+        <div class="ft_nav_item"><a href="">Programmes d’entrainement</a></div>
+        <div class="ft_nav_item"><a href="">Nous rejoindre</a></div>
+        <div class="ft_nav_item"><a href="">Contact</a></div>
     </div>
     <div class="ft_nav_search">
         <form action="" method="get">
-            <input type="search" name="search" id="search">
-            <span>
-                <img src="" alt="">
-            </span>
+            <input placeholder="search..." type="search" name="search" id="search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            
         </form>
     </div>
     <div class="ft_nav_account">
         <div class="ft_nav_account_notification">
-            <img src="" alt="">
+            <i class="fa-solid fa-bell"></i>
         </div>
-        <span>Mon Compte</span>
-        <div class="ft_nav_account_img"><img src="" alt=""></div>
+        <span>Connexion</span>
+        <div class="ft_nav_account_img"><i class="fa-solid fa-user"></i></div>
     </div>
 </nav>
