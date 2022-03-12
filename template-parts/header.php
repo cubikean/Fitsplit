c0fb22ff711c835c6f9b9287662163513fa501e5
cubikean <hugochatigny@gmail.com>
2022-03-12 17:35:08 +0100

    Menu - Update

--- template-parts/header.php
+++ template-parts/header.php
@@ -1,23 +1,38 @@
 <nav class="ft_nav">
     <div class="ft_nav_logo"><img src="https://picsum.photos/200/70" alt=""></div>
     <div class="ft_nav_items">
-        <div class="ft_nav_item"><a href="">Les coachs</a></div>
-        <div class="ft_nav_item"><a href="">Programmes d’entrainement</a></div>
-        <div class="ft_nav_item"><a href="">Nous rejoindre</a></div>
-        <div class="ft_nav_item"><a href="">Contact</a></div>
+        <ul>
+            <li class="menu">
+                <a href="">Parcourir</a>
+                <ul class="menu_toggle">
+                    <li><a href="">programme d'entrainement</a></li>
+                    <li><a href="">programme d'entrainement</a></li>
+                    <li><a href="">programme d'entrainement</a></li>
+                    <li><a href="">programme d'entrainement</a></li>
+                </ul>
+            </li>
+            <li><a href="">programme d'entrainement</a></li>
+            <li><a href="">programme d'entrainement</a></li>
+            <li><a href="">programme d'entrainement</a></li>
+            <li><a href="">programme d'entrainement</a></li>
+        </ul>
     </div>
-    <div class="ft_nav_search">
-        <form action="" method="get">
-            <input placeholder="search..." type="search" name="search" id="search">
-            <i class="fa-solid fa-magnifying-glass"></i>
-            
-        </form>
-    </div>
-    <div class="ft_nav_account">
-        <div class="ft_nav_account_notification">
-            <i class="fa-solid fa-bell"></i>
+    <!-- <div>
+
+        <div class="ft_nav_search">
+            <form action="" method="get">
+                <input placeholder="search..." type="search" name="search" id="search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+
+            </form>
         </div>
-        <span>Connexion</span>
-        <div class="ft_nav_account_img"><i class="fa-solid fa-user"></i></div>
-    </div>
+        <div class="ft_nav_account">
+            <div class="ft_nav_account_notification">
+                <i class="fa-solid fa-bell"></i>
+            </div>
+            <span>Connexion</span>
+            <div class="ft_nav_account_img"><i class="fa-solid fa-user"></i></div>
+        </div>
+    </div> -->
+
 </nav>
