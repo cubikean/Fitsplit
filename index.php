dbc44beff485885d06a36374b3e844c0f5092422
cubikean <hugochatigny@gmail.com>
2022-04-09 11:32:19 +0200

    Add scss minxins
    Ajout des minxins scss pour les boutons, update de la HomePage

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +9,62 @@
     <link rel="stylesheet" href="assets/scss/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-<?php include 'template-parts/header.php';?>
-<script src="assets/js/header.js"></script>
 
+<body>
+    <?php include 'template-parts/header.php'; ?>
+
+    <section class="ft_home_container">
+        <div class="ft_home_content">
+            <div class="ft_home_content_left">
+                <div class="ft_home_content_left_sentences">
+                    <h2>Fitsplit</h2>
+                    <span>c'est</span>
+                </div>
+                <div class="ft_home_content_left_infos">
+                    <div class="ft_home_content_left_infos_single">
+                        <h3>150</h3>
+                        <h4>Coachs</h4>
+                    </div>
+                    <div class="ft_home_content_left_infos_single">
+                        <h3>361</h3>
+                        <h4>Programmes</h4>
+                    </div>
+                    <div class="ft_home_content_left_infos_single">
+                        <h3>13.99€</h3>
+                        <h4>Par mois</h4>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="ft_home_content_right">
+                <div class="ft_home_content_right_sentences_first">
+                    <h1>Le sport depuis chez vous !</h1>
+                </div>
+                <img src="/assets/img/run_img_home.svg" alt="">
+                <div class="ft_home_content_right_sentences_second">
+                    <h2>Avec des coachs certifiés</h2>
+                </div>
+
+            </div>
+            <div class="ft_btn">
+                <a href="#">Découvrir</a>
+            </div>
+
+        </div>
+        <div class="ft_mouse">
+            <div class="ft_mouse_scroller">
+
+            </div>
+        </div>
+    </section>
+    <section class="ft_home_container">
+    </section>
+
+
+
+
+    <script src="assets/js/header.js"></script>
 </body>
+
 </html>
